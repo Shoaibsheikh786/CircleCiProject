f7213026b924870ce751063e8dfe84563844dd1c
Muneer Ahmad <shoaib786asme@gmail.com>
2024-04-03 22:40:43 +0530

    second commit with yml

--- src/base/TestCase.java
+++ src/base/TestCase.java
@@ -3,6 +3,8 @@ package base;
 public class TestCase {
 	public static void main(String[] args) {
 		System.out.println("This is my code for circle ci");
+		
+		System.out.println("This is my code for circle ci 2");
 	}
 
 }
